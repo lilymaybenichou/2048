dcfd70c2ff2d57abda637f1f6aa9ffbfcd992435
uvsq22203153 <115069935+uvsq22203153@users.noreply.github.com>
2023-03-16 12:47:07 +0100

    Avancement Amîn 4.0

--- fichieramîn.py
+++ fichieramîn.py
@@ -7,30 +7,60 @@ from random import*
 tableau = np.array([[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]])
 
 #fonction
-def fermer_fenetre():
+def fermer_fenetre(): #Marche (100%)
     fenetre.destroy()
 
-def ouvrir():
+def ouvrir(): #Marche (100%)
     ouvrir_fichier = filedialog.askopenfilename(title=" ouvrir un fichier")
 
-def sauvegarder():
+def sauvegarder(): #Marche (100%)
     ouvrir_fichier = filedialog.asksaveasfile(title =" sauvagarder la partie")
 
-def play():
-    c=random.randint(0,6)
-    alea=tk.Label(fenetre,text=c,bg="#FFC584")
-    alea.grid(row=0,column=4)
+from random import*
 
-def left():
+def play(): #Marche (75%)
+    hasardrow1=randint(4,7)
+    hasardcolumn1=randint(3,6)
+    hasardrow2=randint(4,7)
+    hasardcolumn2=randint(3,6)
+    casedebut1=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+    casedebut1.grid(row=int(hasardrow1),column=int(hasardcolumn1))
+    casedebut2=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+    casedebut2.grid(row=int(hasardrow2),column=int(hasardcolumn2))
+    debut1=tk.Label(fenetre, text="2",bg="#FFC0C0",height=3,width=6,font=("Helvetica", 20))
+    debut1.grid(row=int(hasardrow1),column=int(hasardcolumn1))
+    debut2=tk.Label(fenetre, text="2",bg="#FFC0C0",height=3,width=6,font=("Helvetica", 20))
+    debut2.grid(row=int(hasardrow2),column=int(hasardcolumn2))
+    bouton1=tk.Button(fenetre,text='Try Again',height=2,width=12,bg='Blue',command=tryagain)
+    bouton1.grid(row=1, column=0, columnspan=2, rowspan=2)
+    
+def tryagain(): #Marche pas (25%)
+    hasardrow1=randint(4,7)
+    hasardcolumn1=randint(3,6)
+    hasardrow2=randint(4,7)
+    hasardcolumn2=randint(3,6)
+    casedebut1=tk.Canvas(width= 0,height=0,bg="")
+    casedebut1.grid(row=int(hasardrow1),column=int(hasardcolumn1))
+    casedebut2=tk.Canvas(width=0,height=0,bg="")
+    casedebut2.grid(row=int(hasardrow2),column=int(hasardcolumn2))
+    debut1=tk.Label(fenetre, text="",bg="",height=0,width=0)
+    debut1.grid(row=int(hasardrow1),column=int(hasardcolumn1))
+    debut2=tk.Label(fenetre, text="",bg="",height=0,width=0)
+    debut2.grid(row=int(hasardrow2),column=int(hasardcolumn2))
+    bouton1=tk.Button(fenetre,text='Play',height=2,width=12,bg='green',command=play)
+    bouton1.grid(row=1, column=0, columnspan=2, rowspan=2)
+    
+
+def left(): #Marche pas (0%)
     pass
 
-def right():
+def right(): #Marche pas (0%)
     pass
 
-def up():
+def up(): #Marche pas (0%)
     pass
 
-def down():
+def down(): #Marche pas (0%)
     pass
 
 #fenetre
@@ -39,14 +69,20 @@ fenetre.title("Jeu du 2048")
 fenetre.geometry('700x600')
 fenetre['bg']='#FFC584'
 
+#zonetexte
+affi1=tk.Label(fenetre, text="°",fg="#FFC584",bg="#FFC584",height=3,width=3)
+affi1.grid(column=1, row=0)
+affi1=tk.Label(fenetre, text="°",fg="#FFC584",bg="#FFC584",height=3,width=3)
+affi1.grid(column=8, row=0)
+
 #bouton
-bouton1=tk.Button(fenetre,text='Play',height=2,width=5,bg='green',command=play)
+bouton1=tk.Button(fenetre,text='Play',height=2,width=12,bg='green',command=play)
 bouton1.grid(row=1, column=0, columnspan=2, rowspan=2)
-bouton6=tk.Button(fenetre,text='Exit',height=2,width=5,bg='red',command=fermer_fenetre)
+bouton6=tk.Button(fenetre,text='Exit',height=2,width=12,bg='red',command=fermer_fenetre)
 bouton6.grid(row=1, column=9, columnspan=2)
-bouton7=tk.Button(fenetre,text='Save',height=2,width=5,bg='grey',command=sauvegarder)
+bouton7=tk.Button(fenetre,text='Save',height=2,width=12,bg='grey',command=sauvegarder)
 bouton7.grid(row=2, column=9, columnspan=2)
-bouton8=tk.Button(fenetre,text='Load',height=2,width=5,bg='grey',command=ouvrir)
+bouton8=tk.Button(fenetre,text='Load',height=2,width=12,bg='grey',command=ouvrir)
 bouton8.grid(row=3, column=9, columnspan=2)
 
 #tableau
