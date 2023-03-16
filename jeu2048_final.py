f84dce001925c2b6c9e83f91db935b3b373ec31e
uvsq22203153 <115069935+uvsq22203153@users.noreply.github.com>
2023-03-16 10:05:55 +0100

    Test

--- jeu2048_final.py
+++ jeu2048_final.py
@@ -1,7 +1,67 @@
 #fichier final du jeu
 
 #amîn
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import simpledialog
+import numpy as np
+from random import*
 
+tableau = np.array([[0, 0, 0, 0], [0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]])
+
+def fermer_fenetre():
+    fenetre.destroy()
+
+def play():
+    c=random.randint(0,6)
+    r=random.randint(0,7)
+    c1=tk.Label(fenetre,text="2",bg="black")
+    c1.grid(row=c,column=r)
+
+fenetre=tk.Tk()
+fenetre.title("Jeu du 2048")
+fenetre.geometry('700x600')
+fenetre['bg']='#FFC584'
+
+zonaffi=tk.Label(fenetre,text="°",bg="#FFC584")
+zonaffi.grid(row=0,column=0)
+zonaffi=tk.Label(fenetre,text="°",bg="#FFC584")
+zonaffi.grid(row=0,column=0)
+zonaffi3=tk.Label(fenetre,text="°",bg="#FFC584")
+zonaffi3.grid(row=0,column=7)
+
+canevas1=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas1.grid(row=4,column=3)
+canevas2=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas2.grid(row=4,column=4)
+canevas2=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas2.grid(row=4,column=5)
+canevas4=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas4.grid(row=4,column=6)
+canevas5=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas5.grid(row=5,column=3)
+canevas6=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas6.grid(row=5,column=4)
+canevas7=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas7.grid(row=5,column=5)
+canevas8=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas8.grid(row=5,column=6)
+canevas9=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas9.grid(row=6,column=3)
+canevas10=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas10.grid(row=6,column=4)
+canevas11=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas11.grid(row=6,column=5)
+canevas12=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas12.grid(row=6,column=6)
+canevas13=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas13.grid(row=7,column=3)
+canevas14=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas14.grid(row=7,column=4)
+canevas15=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas15.grid(row=7,column=5)
+canevas16=tk.Canvas(width=100,height=100,bg="#FCE6CC")
+canevas16.grid(row=7,column=6)
 
 
 
