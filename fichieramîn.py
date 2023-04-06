7bf738e23648a2a8bcbf86dfcd3cf2368b72a9d6
uvsq22203153 <115069935+uvsq22203153@users.noreply.github.com>
2023-04-06 20:03:59 +0200

    Avancé Amin 12.0

--- fichieramîn.py
+++ fichieramîn.py
@@ -11,6 +11,7 @@ val_num = {
   "number_color":["#000000","#1A1A1A","#333333","#4D4D4D","#666666","#808080","#9A9A9A","#B3B3B3","#CDCDCD","#E6E6E6","#FFFFFF"],
   "colors_case": ["#FFE6E6","#FFB3B3","#FF8080","#FF4D4D","#FF1A1A","#FF0000","#E60000","#B30000","#800000","#4D0000","#1A0000"]
 }
+
 #fonction
 def fermer_fenetre(): #Marche (100%)
     fenetre.destroy()
@@ -29,6 +30,7 @@ def help(): #Pris de Pio
     label=tk.Label(fenetre2,fg="black",text="Sur une grille de 16 cases on fait bouger les tuiles pour obtenir un 2048")
     label.pack()
     fenetre2.mainloop()
+
 import webbrowser
 
 def partage(): #Pris de Pio
@@ -39,13 +41,13 @@ def avis(): #Pris de Pio
     fenetre6['bg']="orange"
     fenetre6.title("Note du jeu 2048")
     fenetre6.geometry("300x300")
-    label9=tk.Label(fenetre6,text="Notez le jeu 2048 les etoiles dependent de votre appreciation.")
+    label9=tk.Label(fenetre6,text="Notez le jeu 2048 avec les étoiles.")
     label9.pack()
-    app1=tk.Button(fenetre6,text="1 etoile",fg="yellow",bg="green")
-    app2=tk.Button(fenetre6,text="2 etoile",fg="yellow",bg="green")
+    app1=tk.Button(fenetre6,text="5 etoile",fg="yellow",bg="green")
+    app2=tk.Button(fenetre6,text="4 etoile",fg="yellow",bg="green")
     app3=tk.Button(fenetre6,text="3 etoile",fg="yellow",bg="green")
-    app4=tk.Button(fenetre6,text="4etoile",fg="yellow",bg="green")
-    app5=tk.Button(fenetre6,text="5etoile",fg="yellow",bg="green")
+    app4=tk.Button(fenetre6,text="2 etoile",fg="yellow",bg="green")
+    app5=tk.Button(fenetre6,text="1 etoile",fg="yellow",bg="green")
     
     app1.pack()
     app2.pack()
@@ -61,8 +63,20 @@ def play(): #Marche (100%)
     N=["2","2","4","2","2","2","2","2","2"]
     color1=""
     color2=""
+    texte1=""
+    texte2=""
     V1=choice(V)
     V2=choice(N)
+    B1=choice(V)
+    B2=choice(N)
+    if B1=='4':
+        texte1=val_num["number_color"][1]
+    else:
+        texte1=val_num["number_color"][0]
+    if B2=='4':
+        texte2=val_num["number_color"][1]
+    else:
+        texte2=val_num["number_color"][0]
     if V1=='4':
         color1=val_num["colors_case"][1]
     else:
@@ -77,9 +91,9 @@ def play(): #Marche (100%)
     hasardrow2=randint(4,7)
     hasardcolumn2=randint(3,6)
 
-    debut1=tk.Label(fenetre, text=V1,bg=color1,height=3,width=6,font=("Helvetica", 20),bd=2)
+    debut1=tk.Label(fenetre, text=V1,bg=color1,fg=texte1,height=3,width=6,font=("Helvetica", 20),bd=2)
     debut1.grid(row=int(hasardrow1),column=int(hasardcolumn1))
-    debut2=tk.Label(fenetre, text=V2,bg=color2,height=3,width=6,font=("Helvetica", 20),bd=2)
+    debut2=tk.Label(fenetre, text=V2,bg=color2,fg=texte2,height=3,width=6,font=("Helvetica", 20),bd=2)
     debut2.grid(row=int(hasardrow2),column=int(hasardcolumn2))
 
     bouton1=tk.Button(fenetre,text='Try Again',height=2,width=12,bg='Blue',command=tryagain)
@@ -147,16 +161,14 @@ affi1=tk.Label(fenetre, text="°",fg="#AC61B5",bg="#AC61B5",height=3,width=3)
 affi1.grid(column=8, row=0)
 affi1=tk.Label(fenetre, text="°",fg="#AC61B5",bg="#AC61B5",height=3,width=3)
 affi1.grid(column=0, row=0)
+affi1=tk.Label(fenetre, text="°",fg="#AC61B5",bg="#AC61B5",height=3,width=3)
+affi1.grid(row=2, column=9)
 
 #bouton
 bouton1=tk.Button(fenetre,text='Play',height=2,width=12,bg='green',command=play)
 bouton1.grid(row=1, column=1)
 bouton6=tk.Button(fenetre,text='Exit',height=2,width=12,bg='red',command=fermer_fenetre)
 bouton6.grid(row=1, column=9, columnspan=2)
-bouton7=tk.Button(fenetre,text='Save',height=2,width=12,bg='grey',command=sauvegarder)
-bouton7.grid(row=2, column=9, columnspan=2)
-bouton8=tk.Button(fenetre,text='Load',height=2,width=12,bg='grey',command=ouvrir)
-bouton8.grid(row=3, column=9, columnspan=2)
 
 #tableau
 canevas1=tk.Canvas(width=100,height=100,bg="#EFB9F5")
@@ -195,25 +207,26 @@ canevas16.grid(row=7,column=6)
 #Menu (APPARTENANT A PIOOOOOOOO !)
 mon_menu= tk.Menu(fenetre)
 exit=tk.Menu(mon_menu,tearoff=0)
-exit.add_command(label="quitter",command=fermer_fenetre)
+exit.add_command(label="Exit",command=fermer_fenetre)
 aide=tk.Menu(mon_menu,tearoff=0)
-aide.add_command(label="regle du jeu",command=help)
+aide.add_command(label="Rules of 2048",command=help)
 fichier= tk.Menu(mon_menu,tearoff=0)
-fichier.add_command(label="Enregistrer sous",command=save)
-fichier.add_command(label="Partager score",command=partage)
+fichier.add_command(label="Save as",command=sauvegarder)
+fichier.add_command(label="Open files",command=ouvrir)
+fichier.add_command(label="Share score",command=partage)
 option = tk.Menu(mon_menu,tearoff=0)
-option.add_command(label="changer qlqc")
-option.add_command(label=" difficulte ")
+option.add_command(label="Edit")
+option.add_command(label="Level")
 notezlejeu= tk.Menu(mon_menu,tearoff=0)
-notezlejeu.add_command(label="note",command=avis)
+notezlejeu.add_command(label="Note",command=avis)
 rejouer= tk.Menu(mon_menu,tearoff=0)
-rejouer.add_command(label="rejouer")
+rejouer.add_command(label="Try Again",command=tryagain)
 mon_menu.add_cascade(label="Fichier",menu=fichier)
 mon_menu.add_cascade(label="Option",menu=option)
-mon_menu.add_cascade(label="Aide",menu= aide)
+mon_menu.add_cascade(label="Help",menu= aide)
 mon_menu.add_cascade(label="Exit",menu=exit)
 mon_menu.add_cascade(label="Note",menu=notezlejeu)
-mon_menu.add_cascade(label="Rejouer",menu=rejouer)
+mon_menu.add_cascade(label="Retry",menu=rejouer)
 
 fenetre.config(menu=mon_menu)
 
