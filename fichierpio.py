bcaf5d1d62b2f827ae5779ea663569441b8df297
Pio Dubrail <pio.dubrail@ens.uvsq.fr>
2023-04-15 15:42:13 +0200

    o

--- fichierpio.py
+++ fichierpio.py
@@ -21,13 +21,28 @@ label1 = tk.Label(fenetre, text="Welcome to the 2048 Game ", font = ("helvetica"
 
 label1.grid(column=0, row=0) # positionnement du premier widget
 
-    
+
+
+def affichage():
+    """ Modifie le texte d'un label. """
+    global cpt
+    cpt += 1
+    label.config(text="tu as cliqué " + str(cpt)+ " fois")
+
+cpt = 0
+
+label = tk.Label(fenetre, text="texte avant de cliquer sur le bouton",
+                  padx=20, pady=20, font = ("helvetica", "7") 
+                )
+label.grid(row=9, column=0)
+
+ 
 
 
 
     
 
-bouton1=tk.Button(fenetre,text='Play',bg='red')
+bouton1=tk.Button(fenetre,text='Play',bg='red',command=affichage,font=("helvetica",7))
 bouton1.grid(row=1,column=0)
 bouton2=tk.Button(fenetre,text='Left',bg='red')
 bouton2.grid(row=2,column=0)
