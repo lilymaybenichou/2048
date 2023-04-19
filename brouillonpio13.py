0391297526c04176642f8b01ecc0c2e5ca993d05
Pio Dubrail <pio.dubrail@ens.uvsq.fr>
2023-04-19 10:08:56 +0200

    pio

--- brouillonpio13.py
+++ brouillonpio13.py
@@ -1,7 +1,16 @@
 from tkinter import *
 
-root=Tk()
-root.title('Bg Setting')
-img= PhotoImage(file="P.png")
-Label(root,image=img).pack()
-mainloop()
+root = Tk()
+
+# Chargement de l'image
+bg_image = PhotoImage(file="image/file/P.png")
+
+# Création d'un Label avec l'image en arrière-plan
+background_label = Label(root, image=bg_image)
+background_label.place(relwidth=1, relheight=1)
+
+# Ajout de vos widgets (exemple)
+label = Label(root, text="Mon texte")
+label.pack()
+
+root.mainloop()
