2621b605ff0fc75a7e9caee0f651274df93431e5
Pio Dubrail <pio.dubrail@ens.uvsq.fr>
2023-04-23 12:08:42 +0200

    pio

--- fichier en cours.py
+++ fichier en cours.py
@@ -428,7 +428,7 @@ def restart():
     for i in range(4): # la boucle ici permet ajouter ici 4 petite liste 
         grille.append([0] * 4)
     score=0
-    bouton_play=tk.Button(fenetre,text="Play",font=("Helvetica",20),command=fonctionlieeaplay,bg="DodgerBlue3")
+    bouton_play=tk.Button(fenetre,text="Play",font=("Helvetica",20),command=play2,bg="DodgerBlue3")
     bouton_play.grid(row=5,column=4)
     
 
