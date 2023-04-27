04ca0470fa759b46b550aec946da9329b5369638
Pio Dubrail <pio.dubrail@ens.uvsq.fr>
2023-04-27 10:17:06 +0200

    pio

---
+++ Piomusique.py
@@ -0,0 +1,18 @@
+#Fonction pour la création d'une musique 
+from tkinter import * 
+import pygame 
+#Création d'une fenétre
+fenetre=Tk()
+fenetre.title("pio")
+fenetre.geometry("500x400")
+
+pygame.mixer.init()
+#Fonction play, permettant de jouer une musique donnée
+def play():
+    pygame.mixer.music.load("audio/Not_Me.mp3")
+    pygame.mixer.music.play(loops=0)
+#Lorsque l'on clique sur le bouton "Play song", la musique en question est jouée
+my_button = Button(fenetre,text="Play song",font=("Helvetica",32),command=play)
+my_button.pack(pady=20)
+#Pour afficher une fenetre
+fenetre.mainloop()
