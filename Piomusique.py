5c742844f7957bba8bff923d7cdbf3289789d64e
Pio Dubrail <pio.dubrail@ens.uvsq.fr>
2023-04-27 10:38:41 +0200

    pio

--- Piomusique.py
+++ Piomusique.py
@@ -9,7 +9,7 @@ fenetre.geometry("500x400")
 pygame.mixer.init()
 #Fonction play, permettant de jouer une musique donnée
 def play():
-    pygame.mixer.music.load("Musique/Elektronomia_-_Limitless_Loadedsongs.com.mp3")
+    pygame.mixer.music.load("Music/Elektronomia_-_Limitless_Loadedsongs.com.mp3")
     pygame.mixer.music.play(loops=0)
 #Lorsque l'on clique sur le bouton "Play song", la musique en question est jouée
 my_button = Button(fenetre,text="Play song",font=("Helvetica",32),command=play)
