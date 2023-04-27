64e53ce77ddd89fcdafb7a2545b7e848ae00c173
uvsq22203153 <115069935+uvsq22203153@users.noreply.github.com>
2023-04-27 23:14:45 +0200

    Mise en ordre

--- PIO2musique.py
+++
@@ -1,8 +0,0 @@
-import winsound
-
-winsound.PlaySound('Music1.wav',winsound.SND_ASYNC)
-
-#Stop window for closing
-delay = input("Press ENTER to finish")
-
-#For Reference only 
--- fichier pour la resolution.py
+++
@@ -1,4 +0,0 @@
-# Technique pour éviter la saturation de la grille 
-for i in range(4):
-    for j in range(4):
-        if 2048 in 
